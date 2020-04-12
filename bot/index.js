b03b35c8a05404c5976b9d53b0c3e4f704c21448
SaucyX <ashah775@hotmail.com>
2020-04-12 17:44:34 -0400

    removed

--- bot/index.js
+++ bot/index.js
@@ -282,31 +282,6 @@ bot.on('message', async msg => {
 			}
             msg.reply("fuck you");
             break;
-			
-		case 'sergal':
-		if (msg.author.id == viktor){
-				const sergembed = new Discord.MessageEmbed()
-                .setTitle("here ya go")
-                .setAuthor("sauceBot", bot.user.avatarURL())
-                .setColor(0x10FFFF)
-				.setDescription('fuck you viktor')
-                .attachFiles('images/sergal/fuckyou.png')
-				.setImage('attachment://fuckyou.png')
-            msg.channel.send(sergembed);
-			
-		}
-		else{
-		const sergembed = new Discord.MessageEmbed()
-                .setTitle("Niggles, The Sergal")
-                .setAuthor("sauceBot", bot.user.avatarURL())
-                .setColor(0x10FFFF)
-				.setDescription('here is your sergal')
-                .attachFiles('images/sergal/niggles.png')
-				.setImage('attachment://niggles.png')
-
-            msg.channel.send(sergembed);
-		}
-		break;
 		
 		case 'votekick':
 		var yes;
