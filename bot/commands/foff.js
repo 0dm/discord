ba79be4d500161f717e22d998654e6640885df9c
SaucyX <ashah775@hotmail.com>
2020-08-24 03:53:24 -0400

    update

--- bot/commands/foff.js
+++ bot/commands/foff.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports = {
-    name: 'foff',
+    name: 'fuckoff',
     description: 'used when this bot is a pain in the ass',
     execute(bot,msg,args,guildConf){
         if (msg.author.id == 215190712063492096) {
