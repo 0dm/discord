e4da1709ebb5a0248ecb9e9e5de46a8cd9b6407b
SaucyX <ashah775@hotmail.com>
2020-09-12 00:43:21 -0400

    Update hopon.js

--- bot/commands/hopon.js
+++ bot/commands/hopon.js
@@ -29,7 +29,7 @@ module.exports = {
                 });
                 
                 return;
-            }
+            } else {msg.reply("Join a voice channel first!");}
         } 
         if (user != msg.author.id) { 
             msg.channel.send({
